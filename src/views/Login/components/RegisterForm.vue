54e8af6ade3d8d9f3e7435e7c939363376ed1533
Felix <felix.mutua@gmail.com>
2023-01-25 10:12:55 +0300

    fix registration array

--- src/views/Login/components/RegisterForm.vue
+++ src/views/Login/components/RegisterForm.vue
@@ -146,7 +146,7 @@ const schema = reactive<FormSchema[]>([
       span: 24
     },
     componentProps: {
-      multiple: false,
+      multiple: true,
       options: RoleOptions,
       style: {
         width: '100%'
