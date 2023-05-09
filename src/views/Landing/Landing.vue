3c1cd479c4fbe087447bf53ed298469454ac7a7a
fnmutua <felix.mutua@gmail.com>
2023-05-09 20:29:54 +0300

    System name to KeSIS

--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -21,7 +21,7 @@
 
   <el-menu-item index="0">
     <!-- <img src="@/assets/imgs/logo.png" class="w-[calc(var(--logo-height)-10px)] h-[calc(var(--logo-height)-10px)]" /> -->
-    KSIS
+    KeSIS
   </el-menu-item>
 
 
