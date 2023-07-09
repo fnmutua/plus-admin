24bfd29ba2e5ecd886a0356c4922a14d58b32011
fnmutua <felix.mutua@gmail.com>
2023-07-09 23:56:28 +0300

    Fixed Piechart

--- src/views/Dashboard/chart-types.ts
+++ src/views/Dashboard/chart-types.ts
@@ -261,6 +261,36 @@ export const pieOptions  = reactive({
   ]
 });
 
+export const xpieOptions  = reactive({
+  title: {
+    text: 'Referer of a Website',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [ ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+});
+
 
 export const toggleFullScreen = (cardId) => {
 
