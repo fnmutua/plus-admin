a0322a22a7d967deb8c81c1c7a6ff0499c10c715
fnmutua <felix.mutua@gmail.com>
2023-10-29 11:37:29 +0300

    rewire the repository

--- src/views/Repository/DocumentsTagged.vue
+++ src/views/Repository/DocumentsTagged.vue
@@ -2,23 +2,9 @@
 <script setup lang="ts">
 import { ContentWrap } from '@/components/ContentWrap'
 import { useI18n } from '@/hooks/web/useI18n'
-import { Table } from '@/components/Table'
-import { getSettlementListByCounty } from '@/api/settlements'
-import { getCountyListApi, getListWithoutGeo } from '@/api/counties'
-import { ElButton, ElBadge, ElSelect, MessageParamsWithType } from 'element-plus'
-import { ElMessage, ElOptionGroup, ElIcon } from 'element-plus'
-import {
-  Position,
-  TopRight,
-  User,
-  Plus,
-  Download,
-  Filter,
-  MessageBox,
-  Edit,
-  InfoFilled,
-  Delete
-} from '@element-plus/icons-vue'
+ import { getSettlementListByCounty } from '@/api/settlements'
+ import { ElButton, ElBadge   } from 'element-plus'
+  
 
 import { ref, reactive, watch, computed } from 'vue'
 import {
@@ -26,14 +12,16 @@ import {
   ElFormItem, ElInput, FormRules, ElDropdown, ElDropdownItem, ElDropdownMenu, ElPopconfirm
 } from 'element-plus'
 import { useRouter } from 'vue-router'
-import exportFromJSON from 'export-from-json'
-import { useAppStoreWithOut } from '@/store/modules/app'
+ import { useAppStoreWithOut } from '@/store/modules/app'
 import { useCache } from '@/hooks/web/useCache'
 import { CreateRecord, DeleteRecord, updateOneRecord, deleteDocument } from '@/api/settlements'
-import { uuid } from 'vue-uuid'
-import type { FormInstance } from 'element-plus'
-import { getFile } from '@/api/summary'
-import xlsx from "json-as-xlsx"
+ import xlsx from "json-as-xlsx"
+
+ import { getSummarybyField, getSummarybyFieldNested, getSummarybyFieldFromInclude, getSummarybyFieldSimple } from '@/api/summary'
+
+ import { getSummarybyFieldFromMultipleIncludes } from '@/api/summary'
+ import { getCountyListApi, getListWithoutGeo } from '@/api/counties'
+ import { getFile } from '@/api/summary'
 
 
 const { wsCache } = useCache()
@@ -43,13 +31,10 @@ const userInfo = wsCache.get(appStore.getUserInfo)
 
 // // Hide buttons if not admin 
 const userIsAdmin = ref(false)
-const showEditButtons = ref(false)
-const documentOwner = ref(false)
-const denyDownload = ref(false)
-
+ 
 if (userInfo.roles.includes("admin")) {
   userIsAdmin.value = true
- }
+}
 
 
 
@@ -87,61 +72,25 @@ console.log("Show Buttons -->", showAdminButtons)
 
 //// ------------------parameters -----------------------////
 //const filters = ['intervention_type', 'intervention_phase', 'settlement_id']
-var filters = []
-var filterValues = []
+ 
 const model = 'document'
 
 const associated_multiple_models = ['project', 'indicator_category_report', 'document_type']
 //// ------------------parameters -----------------------////
 const nested_models = ['settlement', 'county'] // The mother, then followed by the child
-const nested_filter = ['id', [6, 7, 8]] //   column and value of the grandchild. In this case roles. 5=county Admin 
 
 const { t } = useI18n()
 
 
 const groups = ref()
-const groups_backup = ref()
-
-
-
-
-function groupByProperties(objects, properties) {
-  const result = {};
-  for (const object of objects) {
-    // Check if the object has all the required properties
-    if (properties.some((property) => !object[property])) {
-      continue;
-    }
-
-    let currentObject = result;
-
-    for (let i = 0; i < properties.length - 1; i++) {
-      //   const propertyValue = getPropertyByPath(object, properties[i]);
-      const propertyValue = object[properties[i]];
-
-      if (!currentObject[propertyValue]) {
-        currentObject[propertyValue] = {};
-      }
-
-      currentObject = currentObject[propertyValue];
-    }
-
-    // const lastPropertyValue = getPropertyByPath(object, properties[properties.length - 1]);
-    const lastPropertyValue = object[properties[properties.length - 1]];
-
-    if (!currentObject[lastPropertyValue]) {
-      currentObject[lastPropertyValue] = [];
-    }
-
-    currentObject[lastPropertyValue].push(object);
-  }
-  console.log('>>>result<<<<', result)
-
-  
  
 
-   return result;
-}
+const totalDocs = ref()
+const filters = ref([])
+const filterValues = ref([])
+const searchTerm = ref('')
+
+ 
 
 
 
@@ -181,66 +130,7 @@ function flattenArrayOfJSON(arr) {
 
 
 const filterLiveDocs = ref([])
-const getFilteredData = async ([], []) => {
-  loading.value = true
-  const formData = {}
-  formData.limit = 1000
-  formData.page = 1
-  formData.curUser = 1 // Id for logged in user
-  formData.model = model
-  //-Search field--------------------------------------------
-  formData.searchField = 'title'
-  formData.searchKeyword = ''
-  //--Single Filter -----------------------------------------
-
-
-  // - multiple filters -------------------------------------
-  formData.filters = []
-  formData.filterValues = []
-  formData.associated_multiple_models = associated_multiple_models
-  formData.nested_models = nested_models
-
-  //-------------------------
-  //console.log(formData)
-  await getSettlementListByCounty(formData)
-    .then(response => {
-      var flattenedObj = flattenArrayOfJSON(response.data);
-
-
-  //create the subcategories 
-      var filteredObjs = flattenedObj.filter(function (doc) {
-        console.log("userIsAdmin.value",userIsAdmin.value)
-        console.log("createdBy", doc.createdBy === userInfo.id || !doc.protectedFile)
-        
-        if (userIsAdmin.value) {
-           return doc
-        } else {
  
-          return (doc.createdBy === userInfo.id || !doc.protectedFile)
-        }
-   
-});
-
-
-
-      
-      console.log('filteredObjs',filteredObjs)
-
-      groups.value = groupByProperties(filteredObjs, ['document_type.group', 'document_type.type'])
-
-
-      groups_backup.value = groupByProperties(filteredObjs, ['document_type.group', 'document_type.type'])
-
-
-      loading.value = false
-
-    });
-
-
-
-
-
-}
 //console.log('TBL-4f', liveDocs.value)
 
 
@@ -291,102 +181,196 @@ function formatText(str) {
 }
 
 
-
-
-
-
-
-getFilteredData(filters, filterValues)
-
-//getDocumentTypes()
-const searchTerm = ref('')
-function searchObjectsByProperties(obj, props, parents = []) {
-  let matches = [];
-
-  for (let key in obj) {
-    const property = obj[key];
-    if (typeof property === 'object' && property !== null) {
-      if (Array.isArray(property)) {
-        for (let i = 0; i < property.length; i++) {
-          const result = searchObjectsByProperties(property[i], props, [...parents, obj]);
-          const uniqueMatches = result.filter(match => !parents.includes(match) && !matches.includes(match));
-          matches = matches.concat(uniqueMatches);
+// 29-10-2023 // 
+const groups_v2 = ref([])
+function reformatData(data) {
+    return data.reduce((result, item) => {
+        const { group, type, count } = item;
+        if (!result[group]) {
+            result[group] = {};
         }
-      } else {
-        const result = searchObjectsByProperties(property, props, [...parents, obj]);
-        const uniqueMatches = result.filter(match => !parents.includes(match) && !matches.includes(match));
-        matches = matches.concat(uniqueMatches);
-      }
-    } else if (property && props.hasOwnProperty(key) &&
-      property.toString().toLowerCase().includes(props[key].toLowerCase())) {
-      if (!parents.includes(obj) && !matches.includes(obj)) {
-        matches.push(obj);
-      }
-    }
-  }
-
-  return matches;
+        result[group][type] = count;
+        return result;
+    }, {});
 }
 
 
+const getCategoryCounts = async () => {
+
+  const formData = {}
+  formData.model = 'document'
+  formData.summaryField = 'document_type.group'
+  formData.summaryFunction = 'count'
+  //formData.assoc_models = ['county']
+  formData.assoc_models = associated_multiple_models
+  formData.groupFields = ['document_type.group', 'document_type.type']
+ 
+  formData.filterField =[]
+  formData.filterValue =[] 
+  formData.filterOperator = ['eq']
+
+  console.log('Filter FormData : ', formData)
+  const response = await getSummarybyFieldFromMultipleIncludes(formData);
+  console.log('groups...', response)
 
 
+  groups_v2.value=reformatData(response.Total)
 
+  console.log('groups_v2', groups_v2.value)
 
-
-
-const filteredGroups = async () => {
-
-
-  let searchWord = searchTerm.value.toLowerCase()
-
-  const props = { 'name': searchWord, 'settlement.name': searchWord, 'settlement.county.name': searchWord };
-  console.log(props)
-  const result = searchObjectsByProperties(groups_backup.value, props);
-  // const result = filterByPropertyValue(groups_backup.value, "name", searchWord);
-
-  //console.log(result)
-  const filtered = groupByProperties(result, ['document_type.group', 'document_type.type'])
-  console.log('filter', filtered)
-
-
-  return filtered;
+  console.log('groups_v1', groups)
 }
 
+const getFilteredDataV2 = async () => {
+  //loading.value = true
+  const formData = {}
+  formData.limit = 5
+  formData.page = currentPage.value
+  formData.curUser = 1 // Id for logged in user
+  formData.model = model
+  //-Search field--------------------------------------------
+  formData.searchField = 'title'
+  formData.searchKeyword = ''
+  //--Single Filter -----------------------------------------
 
 
-watch(
-  searchTerm,
-  async (newValue, oldValue) => {
-    console.log(`Input value changed from ${oldValue} to ${newValue}`);
-    // Do something else here
-    filterLiveDocs.value = []
-    filteredGroups()
-      .then(res => {
-        // console.log('xxxxxxxxxxxx>>>>>', res)
-        //  groupedDocuments(res)
-        groups.value = res
+  // - multiple filters -------------------------------------
+  formData.filters = filters.value
+  formData.filterValues = filterValues.value
+  formData.associated_multiple_models = associated_multiple_models
+  //formData.nested_models = nested_models
+  //sformData.nested_filter = [filters, filterValues]
+  formData.nested_models = nested_models
+ 
+  //-------------------------
+  console.log(formData)
+  await getSettlementListByCounty(formData)
+    .then(response => {
+
+      var flattenedObj = flattenArrayOfJSON(response.data);
+      //create the subcategories 
+      var filteredObjs = flattenedObj.filter(function (doc) {
+       // console.log("userIsAdmin.value", userIsAdmin.value)
+        console.log("createdBy", doc.createdBy === userInfo.id || !doc.protectedFile)
+
+        if (userIsAdmin.value) {
+          return doc
+        } else {
+
+          return (doc.createdBy === userInfo.id || !doc.protectedFile)
+        }
+
+      });
 
 
-      })
+        console.log(response.total)
+
+      console.log('filteredObjs --v2', filteredObjs)
+      filterLiveDocs.value=filteredObjs
+      totalDocs.value=response.total
+      // groups.value = groupByProperties(filteredObjs, ['document_type.group', 'document_type.type'])
 
 
-    // console.log('filterLiveDocs....', filterLiveDocs.value)
-    // console.log('groups....', groups.value)
-  },
-  {
-    immediate: true
+      // groups_backup.value = groupByProperties(filteredObjs, ['document_type.group', 'document_type.type'])
+
+
+      // loading.value = false
+
+    });
+
+
+
+
+
+}
+
+const docTypes=ref([])
+const getDocumentTypes = async () => {
+ 
+  try {
+    const response = await getListWithoutGeo({
+      params: {
+        curUser: 1,
+        model:'document_type' ,
+        associated_multiple_models: [],
+        searchField: 'name',
+        searchKeyword: '',
+        sort: 'ASC'
+      }
+    });
+    
+    console.log('Doc Types', response.data)
+    docTypes.value=response.data
+   
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-);
+};
+
+getDocumentTypes()
+getCategoryCounts()
+ 
+
+
+
+
+
+
+ 
+
+
+ 
 
 
 const handlePageChange = async (newPage) => {
 
   currentPage.value = newPage;
+  await getFilteredDataV2()
 }
 
-console.log("groups<<<---->>>", groups)
+const handleItemCollapse = async (type) => {
+    // clear daata and filers  before laoding next 
+      filterLiveDocs.value=[]
+      filters.value =[]
+      filterValues.value=[]
 
+      // now run the querry 
+      console.log(`Item "${type}" is uncollapsed.`);
+      const SelectedDocType = docTypes.value.filter(obj => obj.type ==type);
+      console.log(SelectedDocType[0].id)
+
+        filters.value =['category']
+        filterValues.value = [[SelectedDocType[0].id]]
+
+      await getFilteredDataV2()
+}
+
+
+
+function getIconForGroup(groupName) {
+    switch (groupName) {
+      case 'Reports':
+        return 'ion:document-outline';
+      case 'Checklists':
+        return 'material-symbols:checklist';
+      case 'Maps':
+        return 'ri:road-map-line';
+      case 'Data':
+        return 'material-symbols:chart-data-outline';
+      case 'Engineering':
+        return 'ion:document-outline';
+      case 'Plans':
+        return 'carbon:heat-map-03';
+      default:
+        return 'material-symbols:linked-camera-outline';
+    }
+  }
+
+
+
+
+
+ 
 </script>
 
 <template>
@@ -395,41 +379,36 @@ console.log("groups<<<---->>>", groups)
     element-loading-text="Getting the documents.......">
     <el-input v-model="searchTerm" placeholder="Search documents by name/settlement/format" class="search-input" />
     <el-collapse accordion>
-      <el-collapse-item v-for="(formats, category) in groups" :key="category">
-
+      <el-collapse-item v-for="(group, groupName) in groups_v2" :key="groupName">
         <template #title>
-          <Icon icon="material-symbols:folder-open-outline" class="collapsible-header-icon  " width="48" />
-          <span class="collapsible-header-text">{{ formatText(category) }}</span>
-
-
-
-        </template>
-
-        <el-collapse accordion>
-          <el-collapse-item v-for="(docs, format) in formats" :key="format">
-            <template #title>
-              <Icon
-v-if="category === 'Report'" icon="ion:document-outline" color='gray'
-                class="collapsible-nested-header-icon " width="36" />
-              <Icon
-v-if="category === 'Map'" icon="ri:road-map-line" class="collapsible-nested-header-icon "
-                color='green' width="36" />
-              <Icon
-v-if="category === 'Data'" icon="material-symbols:chart-data-outline"
-                class="collapsible-nested-header-icon " color='gray' width="36" />
-              <Icon
-v-if="category === 'Other'" icon="material-symbols:linked-camera-outline"
-                class="collapsible-nested-header-icon " color='gray' width="24" />
-              <Icon
-v-if="category === 'Plan'" icon="carbon:heat-map-03" class="collapsible-nested-header-icon "
-                color='gray' width="24" />
-
-              <span class="format-header-text">{{ formatText(format) }}</span>
-              <el-badge v-if="docs.length > 0" :value="docs.length" class="collapsible-header-badge" />
-
+              <Icon icon="material-symbols:folder-open-outline" class="collapsible-header-icon  " width="48" />
+              <span class="collapsible-header-text">{{ formatText(groupName) }}</span>
             </template>
-            <el-table
-:data="docs.slice((currentPage - 1) * pageSize, currentPage * pageSize)"
+            <el-collapse>
+            <el-collapse-item
+              v-for="(typeCount, type) in group"
+              :key="type"
+            >
+              <template #title>
+                <el-button
+                  class="collapsible-nested-header-button"
+                  type=""
+                  link
+                  @click="handleItemCollapse(type)"
+                >
+                  <Icon
+                    :icon="getIconForGroup(groupName)"
+                    color="gray"
+                    class="collapsible-nested-header-icon"
+                    width="36"
+                  />
+                  {{ formatText(type) }}
+                </el-button>
+                <el-badge :value="typeCount" class="collapsible-header-badge" />
+              </template>
+
+              <el-table
+:data="filterLiveDocs"
               style="width: 100%; margin-left: 30px" size="small">
               <el-table-column label="#" type="index" width="50">
                 <template #default="{ $index }">
@@ -446,15 +425,18 @@ v-if="category === 'Plan'" icon="carbon:heat-map-03" class="collapsible-nested-h
                 </template>
               </el-table-column>
             </el-table>
-            <div class="pagination-wrapper" v-if="docs.length > 5">
+            <div class="pagination-wrapper"  v-if="totalDocs > 5">
               <el-pagination
-:page-size="5" background small layout="prev, pager, next" :total="docs.length"
+:page-size="5" background small layout="prev, pager, next" :total="totalDocs"
                 @current-change="handlePageChange" />
-            </div>
-          </el-collapse-item>
-        </el-collapse>
+
+              </div>
+            </el-collapse-item>
+          </el-collapse>
+
       </el-collapse-item>
     </el-collapse>
+
 
 
   </ContentWrap>
@@ -518,4 +500,11 @@ v-if="category === 'Plan'" icon="carbon:heat-map-03" class="collapsible-nested-h
   margin: 5px 0;
   border-top: 1px solid #dcdfe6;
 }
+
+.pagination-wrapper {
+  margin-top: 5px;
+  display: flex;
+  justify-content: center;
+}
 </style>
+
