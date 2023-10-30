3837a7085b8a1f03bf0404cfec8e1fa46a207db8
fnmutua <felix.mutua@gmail.com>
2023-10-30 09:26:05 +0300

    Fix map

--- src/views/Settlement/Sett.vue
+++ src/views/Settlement/Sett.vue
@@ -118,12 +118,12 @@ const userInfo = wsCache.get(appStore.getUserInfo)
 const showAdminButtons = ref(false)
 const showEditButtons = ref(false)
 
-if (userInfo.roles.includes("admin")) {
+if (userInfo.roles.includes("admin") ||(userInfo.roles.includes("super_admin")  )) {
   showAdminButtons.value = true
 }
 
 // Show Edit buttons 
-if (userInfo.roles.includes("staff") || userInfo.roles.includes("admin")
+if (userInfo.roles.includes("staff") || userInfo.roles.includes("admin") || (userInfo.roles.includes("super_admin"))  
   || userInfo.roles.includes("county_admin") || userInfo.roles.includes("national_monitoring")) {
   showEditButtons.value = true;
 }
