1edf3216aa2671c1dcaad0dcff6e7435efb8229d
fnmutua <felix.mutua@gmail.com>
2025-02-11 07:15:28 +0300

    try fix scrollbar

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -66,29 +66,32 @@ export default defineComponent({
 })
 </script>
 
-<style lang="less" scoped>
+<!-- <style lang="less" scoped>
 @prefix-cls: ~'@{namespace}-layout';
-
-// /* Customize the el-scrollbar's draggable part (thumb) */
-// :deep(.el-scrollbar__thumb) {
-//   background-color: red;  /* Set the scrollbar thumb (draggable part) color to red */
-//   border-radius: 10px;  /* Optional: round the thumb edges */
-// }
-
-// /* Customize the el-scrollbar track (the area where the thumb slides) */
-// :deep(.el-scrollbar__track) {
-//   background-color: #f1f1f1;  /* Optional: light color for the track */
-//   border-radius: 10px;  /* Optional: round the track edges */
-// }
-
+ 
 /* Customize el-scrollbar bar (background of the scrollbar) */
 :deep(.el-scrollbar__bar) {
   width: 12px;  /* Make scrollbar thicker */
   height: 100% !important; /* Ensure scrollbar takes up the full height */
 }
 
-/* Add hover effect for the thumb */
-// :deep(.el-scrollbar__thumb:hover) {
-//   background-color: darkred; /* Darker thumb color on hover */
-// }
+ 
+</style> -->
+
+
+<style lang="less" scoped>
+@prefix-cls: ~'@{namespace}-layout';
+
+.@{prefix-cls} {
+  background-color: var(--app-content-bg-color);
+ 
+  :deep(.@{elNamespace}-scrollbar__bar) {
+  width: 12px;  /* Make scrollbar thicker */
+  
+}
+
+}
+/* Customize el-scrollbar bar (background of the scrollbar) */
+
+
 </style>
