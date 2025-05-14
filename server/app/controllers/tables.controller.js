d13d27f013848824bed75d68dcae5dc1272bfb21
fnmutua <felix.mutua@gmail.com>
2025-05-14 08:08:04 +0300

    fix imports docs

--- server/app/controllers/tables.controller.js
+++ server/app/controllers/tables.controller.js
@@ -4417,7 +4417,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 100 * 1024 * 1024, // 100MB limit (adjust as needed)
+    fileSize: 1 * 1024 * 1024 * 1024, // 1GB limit
   },
 });
 
