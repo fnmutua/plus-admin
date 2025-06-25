46aa464a978b95ae25b7a454e72ae3162a02cba8
fnmutua <felix.mutua@gmail.com>
2025-06-25 08:39:23 +0300

    cors

--- production.js
+++ production.js
@@ -9,10 +9,19 @@ const https = require('https');
  
 
 
-app.use(cors());
+//app.use(cors());
 
 
- 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // ✅ only once
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-access-token');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // handle preflight
+  }
+  next();
+});
+
 
 
 const path = require('path');
