1c808197480d2bc9f839a6f12d8c301867ef9eff
fnmutua <felix.mutua@gmail.com>
2025-08-31 09:04:47 +0300

    add hhtps websocket

--- startStopServer.sh
+++ startStopServer.sh
@@ -1,8 +1,7 @@
  sudo pm2 stop production
  sudo pm2 delete production
  sudo pm2 start production.js
- sudo pm2 start server/websocket-chat.js
-
+ 
 
 sudo pm2 stop server/websocket-chat-https.js
 sudo pm2 delete server/websocket-chat-https.js
