27287ea2c98ab7702892c2fcb60c78c621f0bde0
fnmutua <felix.mutua@gmail.com>
2025-11-18 07:05:05 +0300

    fix form

--- src/views/Grievances/GrievanceDetails.vue
+++ src/views/Grievances/GrievanceDetails.vue
@@ -2977,13 +2977,19 @@ width="340"
                     </el-form-item>
                   </el-col>
             <el-col :xs="24" :sm="24" :md="24" :lg="24">
-              <el-form-item id="btn13" label="Is this a GBV-related complaint?">
-                <el-switch v-model="grmForm.isgbv" />
+              <el-form-item id="btn13" label="Is this a GBV-related complaint?" prop="isgbv">
+                <el-select v-model="grmForm.isgbv" placeholder="Select option" style="width: 100%;" filterable>
+                  <el-option label="No" :value="false" />
+                  <el-option label="Yes" :value="true" />
+                </el-select>
               </el-form-item>
             </el-col>
             <el-col :xs="24" :sm="24" :md="24" :lg="24">
-              <el-form-item id="btn13" label="Is this complaint currently in court?">
-                <el-switch v-model="grmForm.isInCourt" />
+              <el-form-item id="btn13a" label="Is this complaint currently in court?" prop="isInCourt">
+                <el-select v-model="grmForm.isInCourt" placeholder="Select option" style="width: 100%;" filterable>
+                  <el-option label="No" :value="false" />
+                  <el-option label="Yes" :value="true" />
+                </el-select>
               </el-form-item>
             </el-col>
           </el-row>
