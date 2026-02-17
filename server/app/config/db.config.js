0cd8f3feda81ce62abcd5227c3b74f63d86b0f90
fnmutua <felix.mutua@gmail.com>
2026-02-17 15:18:00 +0300

    added changes to cliamte rsi

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: process.env.VUE_APP_DB_HOST,
   USER: process.env.VUE_APP_USER,
   PASSWORD: process.env.VUE_APP_PASSWORD,
-  DB: process.env.VUE_APP_DB,
+  DB: "kesmis",
   PORT:process.env.VUE_APP_DB_PORT,
   dialect: "postgres",
   pool: {
