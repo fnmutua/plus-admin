f84fcc994859e75138b2fbea85068c85279bef4e
fnmutua <felix.mutua@gmail.com>
2026-03-03 14:31:12 +0300

    fixes

--- server/app/models/powerline.js
+++ server/app/models/powerline.js
@@ -69,7 +69,7 @@ module.exports = function(sequelize, DataTypes) {
     },
 
     geom: {
-      type: DataTypes.GEOMETRY('MULTILINESTRING', 4326),
+      type: DataTypes.GEOMETRY('GEOMETRY', 4326),
       allowNull: true
     }
   }, {
